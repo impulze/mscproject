7ed226a25b8a1ae6317bd8bdc34f68071bc0d87d
d_miersw <d_miersw@a19d9a54-91ed-43ab-aa6c-052ac356a52f>
2015-06-22 23:16:13 +0000

    fix parameter renderer
    git-svn-id: https://svn.vs.cs.hs-rm.de/svn/d_miersw-bsc/branches/mscproject@46 a19d9a54-91ed-43ab-aa6c-052ac356a52f

--- eclipse_workspace_luna/HZG Editor/src/de/hzg/editor/ParameterTableCellRenderer.java
+++ eclipse_workspace_luna/HZG Editor/src/de/hzg/editor/ParameterTableCellRenderer.java
@@ -1,17 +1,27 @@
 package de.hzg.editor;
 
 import java.awt.Component;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class ParameterTableCellRenderer extends DefaultTableCellRenderer {
 	private static final long serialVersionUID = 6916898139635778985L;
-	
-	public ParameterTableCellRenderer() {
-	}
+	private static NumberFormat formatter = new DecimalFormat("0.00000E0");
 
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		final double doubleValue = (Double)value;
+		final int intValue = (int)doubleValue;
+		final String convertedValue;
+
+		if (intValue == doubleValue) {
+			convertedValue = Integer.toString(intValue);
+		} else {
+			convertedValue = formatter.format(doubleValue);
+		}
+
+		return super.getTableCellRendererComponent(table, convertedValue, isSelected, hasFocus, row, column);
 	}
 }
