7d6919d42b3f48034f416d56d0504cbb354471ba
Daniel Mierswa <daniel.b.mierswa@student.hs-rm.de>
2015-07-03 12:25:17 +0200

    use combobox in EditSensorDialog

--- eclipse_workspace_luna/HZG Editor/src/de/hzg/editor/EditSensorDialog.java
+++ eclipse_workspace_luna/HZG Editor/src/de/hzg/editor/EditSensorDialog.java
@@ -3,14 +3,9 @@ package de.hzg.editor;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
-import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import de.hzg.measurement.SensorDescription;
@@ -23,63 +18,19 @@ public class EditSensorDialog extends EditDialog<SensorDescription> {
 		super(owner, "Select sensor to edit");
 
 		final JLabel sensorNameLabel = new JLabel("Sensor name:");
-		final JTextField sensorNameTextField = new JTextField();
+		final SensorDescriptionComboBox sensorDescriptionComboBox = new SensorDescriptionComboBox(owner, sessionFactory);
 
-		sensorNameTextField.setColumns(20);
-		sensorNameLabel.setLabelFor(sensorNameTextField);
+		sensorNameLabel.setLabelFor(sensorDescriptionComboBox);
 
 		getContentPanel().add(sensorNameLabel);
-		getContentPanel().add(sensorNameTextField);
-
-		final Window usedOwner = owner;
-		final SessionFactory usedSessionFactory = sessionFactory;
+		getContentPanel().add(sensorDescriptionComboBox);
 
 		setOKListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				final Session session = usedSessionFactory.openSession();
-				final String name = sensorNameTextField.getText();
-				final List<SensorDescription> result;
+				EditSensorDialog.this.sensorDescription = (SensorDescription)sensorDescriptionComboBox.getSelectedItem();
 
-				try {
-					@SuppressWarnings("unchecked")
-					final List<SensorDescription> tempResult = (List<SensorDescription>)session
-						.createQuery("FROM SensorDescription WHERE name = :name")
-						.setParameter("name", name)
-						.list();
-
-					result = tempResult;
-					handleResult(result, name);
-				} catch (Exception exception) {
-					final String[] messages = { "Sensor could not be loaded.", "An exception occured." };
-					final JDialog dialog = new ExceptionDialog(usedOwner, "Sensor not loaded", messages, exception);
-					dialog.pack();
-					dialog.setLocationRelativeTo(usedOwner);
-					dialog.setVisible(true);
-					return;
-				} finally {
-					session.close();
-				}
-			}
-
-			public void handleResult(List<SensorDescription> result, String name) {
-				if (result.size() == 0) {
-					final String message = String.format(
-						"The sensor with the name '%s' is not in the database.\n" +
-						"Try to look it up via 'Edit -> List sensors'.",
-						name);
-					JOptionPane.showMessageDialog(EditSensorDialog.this, message, "Sensor not found", JOptionPane.ERROR_MESSAGE);
-				} else if (result.size() != 1) {
-					final String message = String.format(
-							"The sensor with the name '%s' occurs multiple times in the database.\n" +
-							"This is a program error and needs to be fixed manually by accessing the database.",
-							name);
-					JOptionPane.showMessageDialog(EditSensorDialog.this, message, "Duplicate sensor", JOptionPane.ERROR_MESSAGE);					
-				} else {
-					sensorDescription = result.get(0);
-					sensorDescription.initSensorDescription();
-					setVisible(false);
-					dispose();
-				}
+				setVisible(false);
+				dispose();
 			}
 		});
 	}
