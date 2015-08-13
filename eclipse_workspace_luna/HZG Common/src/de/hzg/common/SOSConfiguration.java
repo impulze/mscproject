459cc862e8b82cae5fc454c4dd799f0e0736c5f7
d_miersw <d_miersw@a19d9a54-91ed-43ab-aa6c-052ac356a52f>
2015-08-13 03:15:53 +0000

    add sos configuration
    git-svn-id: https://svn.vs.cs.hs-rm.de/svn/d_miersw-bsc/branches/mscproject@102 a19d9a54-91ed-43ab-aa6c-052ac356a52f

---
+++ eclipse_workspace_luna/HZG Common/src/de/hzg/common/SOSConfiguration.java
@@ -0,0 +1,58 @@
+package de.hzg.common;
+
+public class SOSConfiguration {
+	String procedureIdentifierPrefix;
+	String offeringIdentifierPrefix;
+	String offeringName;
+	String featureOfInterestIdentifierPrefix;
+	String featureOfInterestName;
+	String observablePropertyIdentifierPrefix;
+	
+	public String getProcedureIdentifierPrefix() {
+		return procedureIdentifierPrefix;
+	}
+
+	public void setProcedureIdentifierPrefix(String procedureIdentifierPrefix) {
+		this.procedureIdentifierPrefix = procedureIdentifierPrefix;
+	}
+
+	public String getOfferingIdentifierPrefix() {
+		return offeringIdentifierPrefix;
+	}
+
+	public void setOfferingIdentifierPrefix(String offeringIdentifierPrefix) {
+		this.offeringIdentifierPrefix = offeringIdentifierPrefix;
+	}
+
+	public String getOfferingName() {
+		return offeringName;
+	}
+
+	public void setOfferingName(String offeringName) {
+		this.offeringName = offeringName;
+	}
+
+	public String getFeatureOfInterestIdentifierPrefix() {
+		return featureOfInterestIdentifierPrefix;
+	}
+
+	public void setFeatureOfInterestIdentifierPrefix(String featureOfInterestIdentifierPrefix) {
+		this.featureOfInterestIdentifierPrefix = featureOfInterestIdentifierPrefix;
+	}
+
+	public String getFeatureOfInterestName() {
+		return featureOfInterestName;
+	}
+
+	public void setFeatureOfInterestName(String featureOfInterestName) {
+		this.featureOfInterestName = featureOfInterestName;
+	}
+
+	public String getObservablePropertyIdentifierPrefix() {
+		return observablePropertyIdentifierPrefix;
+	}
+
+	public void setObservablePropertyIdentifierPrefix(String observablePropertyIdentifierPrefix) {
+		this.observablePropertyIdentifierPrefix = observablePropertyIdentifierPrefix;
+	}
+}
